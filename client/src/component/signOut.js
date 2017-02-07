75bdbb278109b7f8f490708a68eff7bee8f3b85b
wnghee912 <wnghee91@gmail.com>
2017-02-07 20:42:06 +0900

    updated signout button and fixed grouplist bug

--- client/src/component/signOut.js
+++ client/src/component/signOut.js
@@ -3,28 +3,34 @@ import GoogleLogin from 'react-google-login'
 
 export default class SignOut extends React.Component {
 
-  handleSignOut() {
-    console.log('Hi!')
-    const auth2 = gapi.auth2.getAuthInstance();
-    console.log(auth2);
-    auth2.signOut().then(() => {
-      console.log('User signed out.');
-    });
+  handleSignOut(){
 
-    gapi.client.load('plus', 'v1', () => {
-      const request = gapi.client.plus.people.get({
-        'userId': 'me'
-      });
-      request.execute((resp) => {
-       console.log('Retrieved profile for:' + resp.displayName, resp);
-      });
-    });
+
+    // console.log('Hi!')
+    // const auth2 = gapi.auth2.getAuthInstance();
+    // console.log(auth2);
+    // auth2.signOut().then(() => {
+    //   console.log('User signed out.');
+    // });
+
+    // gapi.client.load('plus', 'v1', () => {
+    //   const request = gapi.client.plus.people.get({
+    //     'userId': 'me'
+    //   });
+    //   request.execute((resp) => {
+    //    console.log('Retrieved profile for:' + resp.displayName, resp);
+    //   });
+    // });
   }
 
   render() {
+    const imgUrl = 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ8_5_tR-TwcWhgMpLeYG7Be1dlXtzaP2VPrnpmEv38IcxxU5nA';
+
     return (
       <div>
-        <input type="button" value="logout" onClick={this.handleSignOut} />
+        <a className="logout" href="http://localhost:3000/logout">
+          Logout
+        </a>
       </div>
     )
   }
