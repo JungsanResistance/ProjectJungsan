79724edbc1bb73511171b561b2cbcc9338ba34df
Ilmo Koo <johnnykoo84@gmail.com>
2017-02-08 13:50:29 +0900

    added rendering added member with warning message

--- client/src/component/addNewGroup.js
+++ client/src/component/addNewGroup.js
@@ -9,11 +9,34 @@ export default class AddNewGroup extends React.Component {
       groupname: '',
       groupmembers: [],
       emailToBeChecked: '',
-      userEmailExist: false,
+      errorMessage: '',
+      fakeMembers: [
+        { name: 'woonghee',
+          email: 'w@gmail.com',
+          active: true,
+      },
+        { name: 'ilmo',
+        email: 'i@gmail.com',
+        active: true,
+      },
+        { name: 'sanghoon',
+          email: 's@gmail.com',
+          active: true,
+      },
+        { name: 'jin',
+        email: 'j@gmail.com',
+        active: true,
+      },
+        { name: 'jun',
+        email: 'jun@gmail.com',
+        active: false,
+      }
+    ]
     };
 
-    this.handleInput = this.handleInput.bind(this)
-    this.handleAddMember = this.handleAddMember.bind(this)
+    this.handleInput = this.handleInput.bind(this);
+    this.handleAddMember = this.handleAddMember.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
   handleInput(event) {
@@ -30,19 +53,54 @@ export default class AddNewGroup extends React.Component {
   }
 
   handleAddMember() {
-    axios.get(`http://localhost:3000/api/group?email=${this.state.emailToBeChecked}`)
-    .then((res) => {
-      // if(res === ) {
-      //
-      // }
-      console.log(res);
+    const filteredData = this.state.fakeMembers.filter((data) => {
+      return data.email === this.state.emailToBeChecked;
+    })
+    if (filteredData.length) {
+      const nextGroupmembers = this.state.groupmembers;
+      if (this.state.groupmembers.indexOf(filteredData[0].name) === -1) {
+        nextGroupmembers.push(filteredData[0].name);
+        this.setState({
+            groupmembers: nextGroupmembers,
+            errorMessage: '',
+        })
+      }
+      else {
+        this.setState({
+          errorMessage: 'user is already added!'
+        })
+      }
+    }
+    else {
+      this.setState({
+        errorMessage: 'user email does not exist!',
+      })
+    }
 
-    });
+    // axios.get(`http://localhost:3000/api/group?email=${this.state.emailToBeChecked}`)
+    // .then((res) => {
+    //   console.log(res);
+    //   //
+    //   // if(res === ) {
+    //   //
+    //   // }
+    //
+    // });
 
+  }
+
+  handleKeyPress(event) {
+    if(event.charCode === 13) {
+      this.handleAddMember();
+    }
   }
 
 
   render() {
+    const groupMembers = this.state.groupmembers.map((data) => {
+      return <p>{data}</p>
+    })
+
     return (
       <div>
         New Group Name:
@@ -51,10 +109,15 @@ export default class AddNewGroup extends React.Component {
         <br />
         <br />
         Add Members:
-        <input type="text" className='addGroupMembers' placeholder='ex) wnghee91@gmail.com'
-          onChange={this.handleInput} />
+        <input type="text" className="addGroupMembers" placeholder='ex) wnghee91@gmail.com'
+          onChange={this.handleInput} onKeyPress={this.handleKeyPress}/>
         <input type="submit" onClick={this.handleAddMember} value="add"/>
-        {errormessage}
+        <p className="errorMessage">{this.state.errorMessage} </p>
+        <br />
+        <br />
+        Following Members will be added to your group: {this.state.groupname}
+        <br />
+        {groupMembers}
       </div>
     )
   }
