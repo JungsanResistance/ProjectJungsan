a8b008ae78a05404ffed67cfcf66f6daab247950
Ilmo Koo <johnnykoo84@gmail.com>
2017-02-09 18:53:22 +0900

    groupname duplicate check and updated group edit, still need to finish groupedit

--- client/src/component/history.js
+++ client/src/component/history.js
@@ -8,41 +8,42 @@ export default class History extends React.Component {
     this.state = {
       history: [],
       imgUrl: '',
+      ispaid: 0,
     };
     this.handleDone = this.handleDone.bind(this);
   }
   componentWillMount() {
-    axios.get('http://ec2-52-78-111-241.ap-northeast-2.compute.amazonaws.com:3000/api/history')
+    axios.get('http://localhost:3000/api/history')
     .then((res) => {
       // console.log(res);
       const getData = JSON.parse(res.data);
-      // console.log(getData);
+      console.log('getData', getData);
       this.setState({
         history: getData,
       });
     });
   }
 
-  handleDone(event) {
-    const check = event.target.className;
-    if (check) {
-      this.setState({
-
-        imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/X_mark.svg/896px-X_mark.svg.png',
-      })
-    }
-    else {
-      this.setState({
-        imgUrl: 'http://findicons.com/files/icons/808/on_stage/128/symbol_check.png',
-      })
-    }
+  handleDone(index) {
+    const nextHistory = [...this.state.history];
+    nextHistory[index].ispaid = !nextHistory[index].ispaid;
+    console.log('nextHistory[index]', nextHistory[index])
+    axios.put(`http://localhost:3000/api/history`, nextHistory[index])
+    .then((res) => {
+      console.log(res);
+      if(res.status === 201)
+        this.setState({
+          history: nextHistory,
+        })
+    });
   }
+
 
   render() {
     const result = [];
     this.state.history.forEach((data, index) => {
       let imgUrl = '';
-
+      console.log(data.ispaid)
       if (data.ispaid) {
         imgUrl = 'http://findicons.com/files/icons/808/on_stage/128/symbol_check.png';
       }
@@ -57,7 +58,8 @@ export default class History extends React.Component {
           <td>{data.date}</td>
           <td>{data.username}</td>
           <td>{data.cost}</td>
-          <td ><img src={this.state.imgUrl} onClick={this.handleDone} className={data.ispaid} /></td>
+          <td ><img src={imgUrl} onClick={() => this.handleDone(index)}
+            className="toggleImg" /></td>
         </tr>);
     });
     return (
