ed9beb20a4b3e559148698bc46ffd0ad0c41855c
HyunTruth <1234hjlee@naver.com>
2017-02-15 13:24:42 +0900

    added isadmin to get('/mypage') group list

--- model/mypage.js
+++ model/mypage.js
@@ -1,8 +1,9 @@
 const mypage = require('../db/mypage');
+const admin = require('../db/admin');
 
 module.exports = {
   get: (req) => {
-    const result = {};
+    const result = { groupList: [] };
     const currentUser = req.session.passport.user;
     return new Promise((resolve, reject) => (resolve()))
     .then(() => (mypage.getTotalSum(currentUser)))
@@ -11,8 +12,21 @@ module.exports = {
       return mypage.getGroupList(currentUser);
     })
     .then((groupList) => {
-      result.groupList = groupList;
-      return result;
+      let JSONgroupList = JSON.stringify(groupList);
+      JSONgroupList = JSON.parse(JSONgroupList);
+      const mapGroupPromise = JSONgroupList.map(group =>
+        admin.checkGroupAdmin(currentUser, group.groupname)
+        .then((isAdmin) => {
+          if (isAdmin.length) group.isadmin = true;
+          else group.isadmin = false;
+          return group;
+        })
+      );
+      return Promise.all(mapGroupPromise)
+      .then((data) => {
+        result.groupList = data;
+        return result;
+      });
     })
     .catch(err => Promise.reject(err));
   },
