4c6adb6e3f77c790fa0dddf78e6049d02593f416
HyunTruth <1234hjlee@naver.com>
2017-02-18 18:35:23 +0900

    hotfix status code mypage

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
   googleAuth: {
     clientID: '142857477193-6pei87bv7g2i1m0il4plcf4t7ebpmvq9.apps.googleusercontent.com',
     clientSecret: 'dWIEbznS6FETPi2obUnQlvPW',
-    callbackURL: 'http://ec2-52-78-111-241.ap-northeast-2.compute.amazonaws.com:3000/auth/google/callback',
+    callbackURL: 'http://localhost:3000/auth/google/callback',
   },
 };
