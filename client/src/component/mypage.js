b18721773d68adaaf16d9c22485189346c84c8b2
wnghee912 <wnghee91@gmail.com>
2017-02-18 15:04:38 +0900

    fixed newTransaction bug and added accept penging rejectButton

--- client/src/component/mypage.js
+++ client/src/component/mypage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, browserHistory } from 'react-router';
 import axios from 'axios';
 import SignOut from './signOut';
 
@@ -11,67 +11,157 @@ export default class Mypage extends React.Component {
       groupList: [],
       sumList: [],
       myEmail: '',
+      pendingUserList: [],
+      testtest: '',
     };
     this.handleDone = this.handleDone.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
   componentWillMount() {
+    this.reset();
+    // const myData = axios.get('http://localhost:3000/api/misc');
+    // const groupData = axios.get('http://localhost:3000/api/mypage');
+    // Promise.all([myData, groupData]).then(res => {
+    //   const myEmailData = JSON.parse(res[0].data)[0].email
+    //   const groupStorage = [];
+    //   const getData = JSON.parse(res[1].data);
+    //   // console.log()
+    //   console.log('getData!!!!!:', getData);
+    //
+    //   getData.groupList.forEach((group) => {
+    //     groupStorage.push(group.groupname);
+    //   });
+    //     console.log(getData.pendingUserList)
+    //
+    //   this.setState({
+    //     groupList: groupStorage,
+    //     sumList: getData.sumList,
+    //     myEmail: myEmailData,
+    //     pendingUserList: getData.pendingUserList,
+    //   });
+    // })
+  }
+
+  reset (){
     const myData = axios.get('http://localhost:3000/api/misc');
     const groupData = axios.get('http://localhost:3000/api/mypage');
-
     Promise.all([myData, groupData]).then(res => {
       const myEmailData = JSON.parse(res[0].data)[0].email
       const groupStorage = [];
       const getData = JSON.parse(res[1].data);
-
+      // console.log()
       console.log('getData!!!!!:', getData);
 
       getData.groupList.forEach((group) => {
         groupStorage.push(group.groupname);
       });
+        console.log(getData.pendingUserList)
 
       this.setState({
         groupList: groupStorage,
         sumList: getData.sumList,
         myEmail: myEmailData,
+        pendingUserList: getData.pendingUserList,
       });
     })
   }
 
-  handleDone(index) {
+  handleDone(event, index) {
     const nextSumList = [...this.state.sumList];
-    const individualTransacionDone = {
-      recipientemail: nextSumList[index].email,
+    //promise callback에 넘기기 위한 변수 지정//
+    const eventValue = event.target.value;
+    let actionType = 'pending';
+
+    console.log(eventValue)
+
+
+    if(event.target.value === '수락') {
+      actionType = 'accept';
+    }
+    else if (event.target.value === '거절') {
+      actionType = 'reject';
+      this.setState({
+
+      })
     }
 
-    axios.put(`http://localhost:3000/api/misc`, individualTransacionDone)
-    .then((res) => {
-      console.log(res);
-      if(res.status === 200) {
-          nextSumList.splice(index, 1)
-          this.setState({
-            sumList: nextSumList,
-          })
+    const individualTransacionDone = {
+      recipientemail: nextSumList[index].email,
+      action: actionType,
+    }
+
+    console.log(individualTransacionDone)
+
+    let answer = true;
+
+    if (event.target.value === '수락') {
+      answer = confirm('정말로 정산을 완료하시겠습니다?');
+    }
+
+    if(answer) {
+
+      axios.put(`http://localhost:3000/api/misc`, individualTransacionDone)
+      .then((res) => {
+        console.log(res);
+        if(res.status === 200) {
+          if (eventValue === '정산요청') {
+            alert('정산중! 상대가 수락하면 정산이 완료됩니다.')
+            browserHistory.push('/mypage')
+          }
+          else if (eventValue === '거절') {
+            browserHistory.push('/mypage')
+          }
+          else if (eventValue === '수락') {
+            nextSumList.splice(index, 1)
+            this.setState({
+              sumList: nextSumList,
+            })
+          }
         }
-    });
+        this.reset();
+      });
+    }
   }
 
   render() {
+
+
     const List = [];
     const {
       groupList,
       sumList,
     } = this.state;
+    let actionButton = '정산요청';
+    let declineButton = '';
+
     if (sumList && this.state.myEmail) {
       sumList.forEach((data, index) => {
-        console.log(data.email, this.state.myEmail)
+        //render all sumlist except me//
         if (data.email !== this.state.myEmail) {
+          this.state.pendingUserList.forEach((member) => {
+            if(data.email === member.applicantemail || data.email === member.acceptoremail) {
+              if (member.applicantemail === this.state.myEmail && member.status === 1) {
+                actionButton = '정산중';
+              }
+              else if (member.status === 1) {
+                actionButton = '수락';
+                declineButton = <button value='거절' onClick={(event) => this.handleDone(event, index)}>거절</button>
+              }
+              else if (member.status === 0) {
+                actionButton = '정산요청';
+              }
+            }
+          })
+
           List.push(
             <tr>
               <td>{data.username}</td>
               <td>{data.email}</td>
               <td>{data.cost}</td>
-              <td><button onClick={() => this.handleDone(index)}>정산완료</button></td>
+              <td>
+                <button value={actionButton} onClick={(event) => this.handleDone(event, index)}>{actionButton}</button>
+                </td>
             </tr>,
           )
         }
@@ -85,13 +175,14 @@ export default class Mypage extends React.Component {
     return (
       <div>
         <ul>
-          <li className="routing"><Link to="history">history</Link></li>
-          <li className="routing"><Link to="transaction">transaction</Link></li>
-          <li className="routing"><Link to="group">newgroup</Link></li>
+          <li className="routing"><Link to="history">History</Link></li>
+          <li className="routing"><Link to="transaction">New event</Link></li>
+          <li className="routing"><Link to="group">New group</Link></li>
         </ul>
         <br />
           <div className="myPage">
           <h1>my Page</h1>
+          <h2>{this.state.myEmail}</h2>
           <table className="sumListTable">
             <tr>
               <th>name</th>
