837a427162394f7d9540a50c598cc5327687ed16
HyunTruth <1234hjlee@naver.com>
2017-02-22 17:27:00 +0900

    url changed!

--- client/src/component/func/failedRedirect.js
+++ client/src/component/func/failedRedirect.js
@@ -6,7 +6,7 @@ export default class FailedRedirect extends React.Component {
 
   componentWillMount() {
     alert('이미 가입된 email 입니다. Login 해 주세요');
-    browserHistory.push('http://ec2-52-78-111-241.ap-northeast-2.compute.amazonaws.com');
+    browserHistory.push('http://oneovern.com');
   }
   render() {
     return (
