564442f3db737042b1525caac29bd267711856bd
HyunTruth <1234hjlee@naver.com>
2017-02-23 09:31:47 +0900

    cleaned up console.log for app.js

--- app.js
+++ app.js
@@ -41,7 +41,6 @@ app.get('*', (req, res, next) => {
         res.sendfile(path.join(__dirname, 'client/dist/index.html'));
       } else return next();
     } else {
-      console.log('why???');
       res.redirect('/');
     }
   } else return next();
