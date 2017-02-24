811bb539d5c3f822dd31811fa9bc099eb50205c8
Johnny Ilmo Koo <johnnykoo84@gmail.com>
2017-02-24 09:48:55 +0900

    Update mypage.js

--- client/src/component/mypage.js
+++ client/src/component/mypage.js
@@ -26,8 +26,8 @@ export default class Mypage extends React.Component {
 
   componentWillMount() {
     this.reset();
-    // const myData = axios.get('https://oneovern/api/misc');
-    // const groupData = axios.get('https://oneovern/api/mypage');
+    // const myData = axios.get('https://oneovern.com/api/misc');
+    // const groupData = axios.get('https://oneovern.com/api/mypage');
     // Promise.all([myData, groupData]).then(res => {
     //   const myEmailData = JSON.parse(res[0].data)[0].email
     //   const groupStorage = [];
@@ -50,8 +50,8 @@ export default class Mypage extends React.Component {
   }
 
   reset() {
-    const myData = axios.get('https://oneovern/api/misc');
-    const groupData = axios.get('https://oneovern/api/mypage');
+    const myData = axios.get('https://oneovern.com/api/misc');
+    const groupData = axios.get('https://oneovern.com/api/mypage');
     Promise.all([myData, groupData]).then((res) => {
       const myEmailData = JSON.parse(res[0].data)[0].email;
       const groupStorage = [];
@@ -104,7 +104,7 @@ export default class Mypage extends React.Component {
     }
 
     if (answer) {
-      axios.put(`https://oneovern/api/misc`, individualTransacionDone)
+      axios.put(`https://oneovern.com/api/misc`, individualTransacionDone)
       .then((res) => {
         if (res.status === 200) {
           if (eventValue === '정산요청') {
