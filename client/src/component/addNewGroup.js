d8265af4232727dc3e5293e10e026dffd8e6220c
HyunTruth <1234hjlee@naver.com>
2017-02-24 11:26:03 +0900

    spinner implemented

--- client/src/component/addNewGroup.js
+++ client/src/component/addNewGroup.js
@@ -26,7 +26,7 @@ export default class AddNewGroup extends React.Component {
   }
 
   componentWillMount() {
-    axios.get('http://localhost:3000/api/misc')
+    axios.get('https://oneovern.com/api/misc')
     .then((res) => {
       console.log(res);
       const logInUserData = JSON.parse(res.data);
@@ -45,7 +45,7 @@ export default class AddNewGroup extends React.Component {
     else {
       console.log("groupname::",this.state.groupname,
       "groupmembers::::", this.state.groupmembers,)
-      axios.post('http://localhost:3000/api/group', {
+      axios.post('https://oneovern.com/api/group', {
         groupname: this.state.groupname,
         groupmembers: this.state.groupmembers,
       })
@@ -79,7 +79,7 @@ export default class AddNewGroup extends React.Component {
 
   handleAddMember() {
     document.body.getElementsByClassName('addGroupMembers')[0].value = '';
-    axios.get(`http://localhost:3000/api/groupedit?target=email&email=${this.state.emailToBeChecked}`)
+    axios.get(`https://oneovern.com/api/groupedit?target=email&email=${this.state.emailToBeChecked}`)
     .then((res) => {
       console.log(res.data);
       const data = JSON.parse(res.data);
@@ -130,7 +130,7 @@ export default class AddNewGroup extends React.Component {
       })
     }
     else {
-      axios.get(`http://localhost:3000/api/groupedit?target=groupname&groupname=${this.state.groupname}`)
+      axios.get(`https://oneovern.com/api/groupedit?target=groupname&groupname=${this.state.groupname}`)
       .then((res) => {
         const data = JSON.parse(res.data);
         if (data.length) {
